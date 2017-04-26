8531ee48c764879c9f8dbbef964cf419e9c8c11a
Kuang-Ming chen <kuangming.chen@dreamlines.de>
2017-04-26 09:10:11 +0200

    Find duplicate in an array

--- array/array.go
+++ array/array.go
@@ -1,5 +1,7 @@
 package array
 
+import "fmt"
+
 // Find pair with given sum in the array
 // Given an unsorted array of integers, find a pair with given sum in it
 // Input: [8, 7, 2, 5, 3, 1], sum = 10
@@ -40,3 +42,49 @@ func FindPairWithGivenSum(input []int, sum int) []int {
 	}
 	return result
 }
+
+// Find sub-array with 0 sum
+// Given an array of integers, check if array contains a sub-array having 0 sum. Also, prints end-points of all such sub-arrays.
+// O(n^2): For each element, loop through all elements in subarray
+// O(n): Traverse the given array, and maintain sum of elements seen so far in a map.
+// If sum is seen before, there exists at-least one sub-array with 0 sum which ends at current index and we print all such sub-arrays
+func SubArrayWithZeroSum(input []int) {
+	// O(n) multi-dimensional map
+	set := map[int][]int{0: []int{-1}}
+	sum := 0
+	for key, val := range input {
+		sum += val
+		if indexes, ok := set[sum]; ok {
+			for _, idx := range indexes {
+				fmt.Println(idx+1, " ... ", key)
+			}
+		}
+		set[sum] = append(set[sum], key)
+	}
+}
+
+// Find a duplicate element in a limited range array
+// Given a limited range array of size n where array contains elements between 1 to n-1 with one element repeating
+// find the duplicate number in it.
+func FindDuplicates(input []int) int {
+	// using XOR
+	result := 0
+	//for _, val := range input {
+	//result ^= val
+	//}
+	//for key, _ := range input {
+	//result ^= key
+	//}
+
+	// using constant space
+	for _, val := range input {
+		if input[val] < 0 {
+			return input[val]
+		}
+		input[val] = input[val] * -1
+	}
+	return result
+}
+
+// Find largest sub-array formed by consecutive integers
+// Given an array of integers, find largest sub-array formed by consecutive integers. The sub-array should contain all distinct values.
--- array/array_test.go
+++ array/array_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestFindPairWithGivenSum(t *testing.T) {
-
 	type test struct {
 		input  []int
 		sum    int
@@ -29,6 +28,69 @@ func TestFindPairWithGivenSum(t *testing.T) {
 			for _, val := range result {
 				So(val, ShouldBeIn, v.output)
 			}
+		}
+	})
+}
+
+func TestSubArrayWithZeroSum(t *testing.T) {
+	type test struct {
+		input  []int
+		output [][]int
+	}
+	//testCases := []*test{
+	//&test{
+	//[]int{4, 2, -3, -1, 0, 4},
+	//[][]int{
+	//[]int{-3, -1, 0, 4},
+	//[]int{0},
+	//},
+	//},
+	//&test{
+	//[]int{3, 4, -7, 3, 1, 3, 1, -4, -2, -2},
+	//[][]int{
+	//[]int{3, 4, -7},
+	//[]int{4, -7, 3},
+	//[]int{-7, 3, 1, 3},
+	//[]int{3, 1, -4},
+	//[]int{3, 1, 3, 1, -4, -2, -2},
+	//[]int{3, 4, -7, 3, 1, 3, 1, -4, -2, -2},
+	//},
+	//},
+	//}
+	input := []int{4, 2, -3, -1, 0, 4}
+	SubArrayWithZeroSum(input)
+	input = []int{3, 4, -7, 3, 1, 3, 1, -4, -2, -2}
+	SubArrayWithZeroSum(input)
+	//Convey("Input should equal output", t, func() {
+	//for _, v := range testCases {
+	//result := SubArrayWithZeroSum(v.input)
+	//for _, val := range result {
+	//So(val, ShouldResemble, v.output)
+	//}
+	//}
+	//})
+}
+
+//Find a duplicate element in a limited range array
+func testFindDuplicates(t *testing.T) {
+	type test struct {
+		input  []int
+		output []int
+	}
+	testCases := []*test{
+		&test{
+			[]int{1, 2, 3, 4, 4},
+			[]int{4},
+		},
+		&test{
+			[]int{1, 2, 3, 4, 2},
+			[]int{2},
+		},
+	}
+	Convey("Input should equal output", t, func() {
+		for _, v := range testCases {
+			result := FindDuplicates(v.input)
+			So(result, ShouldEqual, v.output)
 		}
 	})
 }
